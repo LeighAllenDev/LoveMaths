d8535b6787a5ad512ce26087f2a3dad702fce617
LeighAllenDev <140525595+LeighAllenDev@users.noreply.github.com>
2023-09-20 14:06:46 +0100

    added calculateCorrectAnswer content

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,8 +36,22 @@ function checkAnswer() {
 
 }
 
+/**
+ * gets the operands (numbers) and the operator from the dom,
+ * and returns the correct answer
+ */
 function calculateCorrectAnswer() {
 
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unknown operator ${operator}`);
+        throw `Unknown operator ${operator}. Aborting!`;
+    }
 }
 
 function incrementScore() {
